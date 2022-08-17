53ae5e4ea477b9765b358ce6903726cf998a6463
Ian McPhail <ian@clerk.dev>
2022-08-17 16:28:24 -0400

    Update example

--- src/script.js
+++ src/script.js
@@ -1,35 +1,38 @@
-const frontendApi = 'your-frontend-api'
+const frontendApi = 'your-frontend-api'; // <- Add Frontend API here
 
 const startClerk = async () => {
-    const Clerk = window.Clerk
+  const Clerk = window.Clerk;
 
-    try {
-        // Load Clerk environment & session if available
-        await Clerk.load()
+  try {
+    // Load Clerk environment and session if available
+    await Clerk.load();
 
-        const userButtonEl = document.getElementById('user-button')
-        const authLinks = document.getElementById('auth-links')
+    const userButton = document.getElementById('user-button');
+    const authLinks = document.getElementById('auth-links');
 
-        if (Clerk.user) {
-            // Mount user button component
-            Clerk.mountUserButton(userButtonEl)
-            authLinks.style.display = 'none'
-        } else {
-            authLinks.style.display = 'block'
-        }
-    } catch (err) {
-        console.error('Error starting Clerk: ', err)
+    Clerk.addListener(({ user }) => {
+      // Display links conditionally based on user state
+      authLinks.style.display = user ? 'none' : 'block';
+    });
+
+    if (Clerk.user) {
+      // Mount user button component
+      Clerk.mountUserButton(userButton);
+      userButton.style.margin = 'auto';
     }
-}
+  } catch (err) {
+    console.error('Error starting Clerk: ', err);
+  }
+};
 
-;(() => {
-    const script = document.createElement('script')
-    script.setAttribute('data-clerk-frontend-api', frontendApi)
-    script.async = true
-    script.src = `https://${frontendApi}/npm/@clerk/clerk-js@2/dist/clerk.browser.js`
-    script.addEventListener('load', startClerk)
-    document.body.appendChild(script)
-
-    const noFrontendApiWarning = document.getElementById('no-frontend-api-warning')
-    noFrontendApiWarning.style.display = 'none'
-})()
+(() => {
+  const script = document.createElement('script');
+  script.setAttribute('data-clerk-frontend-api', frontendApi);
+  script.async = true;
+  script.src = `https://${frontendApi}/npm/@clerk/clerk-js@latest/dist/clerk.browser.js`;
+  script.addEventListener('load', startClerk);
+  script.addEventListener('error', () => {
+    document.getElementById('no-frontend-api-warning').hidden = false;
+  });
+  document.body.appendChild(script);
+})();
